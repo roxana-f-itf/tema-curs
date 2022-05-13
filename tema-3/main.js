03fd3802c0847ea27e429fc3077244fee3f06a9f
Roxana F <roxanafrancu85@gmail.com>
2022-05-13 18:10:57 +0300

    tema3

---
+++ tema-3/main.js
@@ -0,0 +1,44 @@
+let her = 'Jane Smith'          // I used the variable name before her, but it showed it is deprecated 
+let sum = '$12,450'
+let car = 'a Toyota Yaris'
+let lot = "is a lot of money"
+let plans = 'plans to buy'
+let bank = "in her bank account"
+
+
+let r = "="
+let result = r.repeat(50)
+
+
+//console.log(her + ' ' + 'has' + ' ' + sum + ' ' + bank);
+let result1 = her + ' ' + 'has' + ' ' + sum + ' ' + bank;
+//console.log(sum + ' ' + lot);
+let result2 = sum + ' ' + lot;
+//console.log(her + ' ' + plans + ' ' + car);
+let result3 = her + ' ' + plans + ' ' + car;
+//console.log(her + ' ' + 'will use her' + ' ' + sum + ' ' + 'to buy' + car);
+let result4 = her + ' ' + 'will use her' + ' ' + sum + ' ' + 'to buy' + car;
+//console.log(sum + ' ' + lot + ' ' + 'for' + ' ' + car);
+let result5 = sum + ' ' + lot + ' ' + 'for' + ' ' + car;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
+
+console.log(result);
+
+console.log(result2);
+console.log(result5);
+console.log(result3);
+console.log(result1);
+console.log(result4);
+
+console.log(result);
+
+console.log(result3);
+console.log(result2);
+console.log(result4);
+console.log(result1);
+console.log(result5);
