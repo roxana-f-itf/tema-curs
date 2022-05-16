35bebc662dac47a65128d1724d3b63b1f74ee750
Roxana F <roxanafrancu85@gmail.com>
2022-05-16 15:55:39 +0300

    fixing

--- tema-3/main.js
+++ tema-3/main.js
@@ -10,7 +10,6 @@ let r = "="
 let result = r.repeat(50)
 
 
-//console.log(her + ' ' + 'has' + ' ' + sum + ' ' + bank);
 let result1 = her + ' ' + 'has' + ' ' + sum + ' ' + bank;
 //console.log(sum + ' ' + lot);
 let result2 = sum + ' ' + lot;
@@ -21,24 +20,77 @@ let result4 = her + ' ' + 'will use her' + ' ' + sum + ' ' + 'to buy' + car;
 //console.log(sum + ' ' + lot + ' ' + 'for' + ' ' + car);
 let result5 = sum + ' ' + lot + ' ' + 'for' + ' ' + car;
 
-console.log(result1);
-console.log(result2);
-console.log(result3);
-console.log(result4);
-console.log(result5);
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
+// console.log(result5);
 
-console.log(result);
+// console.log(result);
 
-console.log(result2);
-console.log(result5);
-console.log(result3);
-console.log(result1);
-console.log(result4);
+// console.log(result2);
+// console.log(result5);
+// console.log(result3);
+// console.log(result1);
+// console.log(result4);
 
-console.log(result);
+// console.log(result);
 
-console.log(result3);
-console.log(result2);
-console.log(result4);
-console.log(result1);
-console.log(result5);
+// console.log(result3);
+// console.log(result2);
+// console.log(result4);
+// console.log(result1);
+// console.log(result5);
+
+
+
+
+let writeParagraph = function (
+    row1,
+    row2,
+    row3,
+    row4,
+    row5,
+) {
+    return row1 + '\n' + row2 + '\n' + row3+ '\n'+ row4 + '\n' + row5
+}
+
+
+console.log(writeParagraph(
+    result1,
+    result2,
+    result3,
+    result4,
+    result5,
+
+));
+
+console.log(writeParagraph(
+    result2,
+    result5,
+    result3,
+    result1,
+    result4,
+
+));
+
+console.log(writeParagraph(
+    result3,
+    result2,
+    result4,
+    result1,
+    result5,
+
+));
+
+
+
+// creating a new <p> element and appending it to the body
+// let paragraph1 = document.createElement('p');
+// paragraph1.innerHTML = result1 +'<br>'+ result2 +'<br>'+ result3 +'<br>'+ result4 +'<br>'+ result5; 
+// document.body.appendChild(paragraph1);
+
+
+// let textElement = document.getElementById('text');
+// textElement.style = "border: solid";
+
