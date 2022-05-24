7d5d7e316cc83e8d3a6d2f0e764cb685b307d04c
Roxana F <roxanafrancu85@gmail.com>
2022-05-24 19:07:35 +0300

    homework no.5

--- main.js
+++ main.js
@@ -1,3 +1,107 @@
 console.log('Hello World');
 console.log('Hello World');
 console.log('Hello World');
+
+
+
+// let weather = 'sunny'
+// if (weather === 'rainy') {
+//     console.log('You need a rain coat.')
+// } else if (weather === 'cloudy') {
+//     console.log('It might be cold, you need a jacket.')
+// } else if (weather === 'sunny') {
+//     console.log('Go out freely.')
+// } else {
+//     console.log('No need for rain coat.')
+// };
+
+// let a = 0
+// if (a > 0) {
+//     console.log(`${a} is a positive number`)
+// } else if (a < 0) {
+//     console.log(`${a} is a negative number`)
+// } else if (a == 0) {
+//     console.log(`${a} is zero`)
+// } else {
+//     console.log(`${a} is not a number`)
+// };
+
+
+// let num = 3;
+// if (num > 0) {
+//     console.log(`${num} is a positive number`)
+// } else {
+//     console.log(`${num} is a negative number`)
+// }
+
+// let num1 = 5;
+// if (num1 > 1) {
+//     console.log('num1 is greater');
+// }
+// else {
+//     console.log('${num1} is less')
+
+// };
+
+
+// let age = 60
+// if (age > 18) {
+//     console.log("yes");
+// }
+// else {
+//     console.log("no");
+// }
+
+// let numbers = [0, 3.14, 9.81, 37, 98.6, 100];
+// console.log('Number of numbers:', numbers.length);
+
+
+// let js = 'JavaScript'
+// const charsInJavaScript = js.split('')
+// console.log(charsInJavaScript);
+
+// let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+// const companies = companiesString.split(',')
+// console.log(companies);
+
+// const fruits = ['banana', 'orange', 'mango', 'lemon']
+// let firstFruit = fruits[0] // we are accessing the first item using its index
+// console.log(firstFruit) // banana
+// secondFruit = fruits[1]
+// console.log(secondFruit) // orange
+// let lastFruit = fruits[3]
+// console.log(lastFruit) // lemon
+// // Last index can be calculated as follows
+// let lastIndex = fruits.length - 1
+// lastFruit = fruits[lastIndex]
+// console.log(lastFruit)  // lemon
+
+// const numbers1 = [1, 2, 3, 4, 5];
+// numbers1[0] = 10      // changing 1 at index 0 to 10
+// numbers1[1] = 20      // changing  2 at index 1 to 20
+// console.log(numbers1) // [10, 20, 3, 4, 5]
+
+
+// const countries = [
+//     'Albania',
+//     'Bolivia',
+//     'Canada',
+//     'Denmark',
+//     'Ethiopia',
+//     'Finland',
+//     'Germany',
+//     'Hungary',
+//     'Ireland',
+//     'Japan',
+//     'Kenya'
+//   ]
+// countries[0] = 'Afghanistan'  // Replacing Albania by Afghanistan
+// let lastIndex1 = countries.length - 1
+// countries[lastIndex1] = 'Korea' // Replacing Kenya by Korea
+// console.log(countries);
+
+
+// const arr = Array() // creates an an empty array
+// console.log(arr)
+// const eightEmptyValues = Array(8) // it creates eight empty values
+// console.log(eightEmptyValues) // [empty x 8]
---
+++ tema-5/tema5.js
@@ -0,0 +1,180 @@
+
+//ex 1.1;
+
+function addition(a, b) {
+    let sum = a + b;
+    return sum;
+};
+
+function substraction(c, d) {
+    let sub = c - d;
+    return sub;
+};
+
+function multiplication(e, f) {
+    let mul = e * f;
+    return mul;
+};
+
+function division(g, h) {
+    let div = g / h
+    return div;
+};
+
+let x = prompt("Please enter a number");
+let r = division(4, x);          // 4/x
+let p = addition(r, 5);          // 4/x+5
+let s = substraction(34, x);   // 34-x
+let t = addition(s, 10);         // 34-x+10
+let u = multiplication(x, t);    //x * (34 - x + 10)
+let v = division(u, 6);          // x * (34 - x + 10) / 6
+let w = substraction(p, v);         //x + 5 - x * (34 - x + 10) / 6
+let q = addition(w, x);          // 4 / x + 5 - x * (34 - x + 10) / 6 + x
+
+console.log(q);
+
+
+
+// ex. 1.2;
+
+function checkPalindrome(string) {
+
+    let len = string.lenght;
+    for (let i = 0; i < len; i++) {
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome.';
+        }
+    }
+    return 'It is a palindrome.';
+}
+let string = prompt('34988943');
+let value = checkPalindrome(string);
+console.log(value);
+
+
+let text = "34988943";
+let text1 = text.charAt(2);
+console.log(text.charAt(2));
+
+
+// ex. 1.3;
+
+function findLongestWord(string) {
+    let str = string.split(' ');
+    let longestWord = 0;
+    let word = null;
+    str.forEach(function (str) {
+        if (longestWord < str.lenght) {
+            longestWord = str.lenght;
+            word = str;
+        };
+    });
+    return word;
+};
+let prop = "JavaScript is the programming language of the Web."
+console.log(findLongestWord(prop));
+
+// ex. 1.4;
+
+let Object = {
+    result: 0,
+    addNumber: function(a, b) {
+      this.result = a + b;
+      return this;
+    },
+
+    substractNumber: function(a) {
+        this.result = this.result - a;
+        return this;
+    },
+  
+    multiplyNumber: function(a) {
+      this.result = this.result * a;
+      return this;
+    },
+   
+    divideNumber: function(a) {
+      this.result = this.result / a;
+      return this;
+    },
+  };
+  
+  Object.addNumber(10, 20).substractNumber(5).multiplyNumber(10).divideNumber(10);
+  console.log(Object.result);
+
+
+
+// ex. 2.1;
+
+let number = prompt("Enter a number");
+function factorial(y) {
+    if (y === 0 || y === 1) {
+        return 1;
+    } else {
+        if (y > 0) {
+            return factorial(y - 1) * y;  // daca numarul nu este pozitiv sau intreg;
+        } else {
+            return '-1';
+        };
+    };
+};
+let resultFactorial = factorial(number);
+console.log(resultFactorial);
+
+//daca numarul primit ca parametru in functie este intreg sau nu.
+//console.log(Math.ceil(resultFactorial));
+//console.log(Math.floor(resultFactorial));
+
+
+if (Math.floor) {
+    Math.floor = function factorial(y) {
+      return //??;
+    };
+  };
+  if (Math.ceil) {
+    Math.ceil = function factorial(y) {
+      return //??;
+    };
+  };
+
+
+   // ex.2.2;
+
+let array = [2, 4, 8, 1, 9, 4, 5, 5, 8, 9, 1, 3, 2];
+function multiply() {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum = sum * array[i];
+    };
+    return sum;
+};
+function addition() {
+    let add = 0;
+    for (let i = 0; i < array.length; i++) {
+        add = add += array[i];
+    };
+    return add;  
+};
+ // apelarea functiei - adunare || inmultire??
+
+
+
+
+   // ex.2.3;
+
+let personObject = {
+    firstName: 'Roxana',
+    lastName: 'Francu',
+    sex: 'f',
+    petOwner: 'yes',
+
+    writePoliteName: function() {
+        let prefix = 'Mr. ';
+        if (personObject.sex === 'f' ) {
+            prefix = 'Mrs. ';
+        };
+
+     console.log(prefix + personObject.firstName + ' ' + personObject.lastName);
+        
+    },
+};
