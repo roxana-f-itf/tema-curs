158c378f2d8154a6babd65084a51aa7fa638d7be
Roxana F <roxanafrancu85@gmail.com>
2022-05-31 18:57:48 +0300

    tema6

---
+++ tema-6/tema6.js
@@ -0,0 +1,32 @@
+//ex. 1.1;
+
+let alertFunction = function() {
+    alert("The first button was pressed.");
+
+};
+
+
+//ex. 1.2; 
+
+let button = document.createElement("button");
+button.innerText = "Second";
+
+button.addEventListener ("click", function() {
+  alert("The second button was pressed.");
+});
+document.body.appendChild(button);
+
+
+//ex. 1.3;
+
+let anotherButton = document.createElement("button");
+anotherButton.innerText = "Start";
+document.body.appendChild(anotherButton);
+
+let buttonFunction = function() {
+  let x = document.createTextNode("Step 1");
+  anotherButton.appendChild(x);
+  document.body.appendChild(anotherButton);
+};
+document.addEventListener("click", buttonFunction);
+
