2c2119251442ce96d7478d335a2d3d6d5aa3c0ec
Roxana F <roxanafrancu85@gmail.com>
2022-06-14 17:13:43 +0300

    finalWork

---
+++ tema-6/tema6.1.js
@@ -0,0 +1,59 @@
+
+let redButton = document.createElement("button");
+redButton.innerText = "RED";
+document.body.appendChild(redButton);
+redButton.style.backgroundColor = "#ff0000";
+redButton.addEventListener("click", function(){
+    alert("People who favor the color red are generally observed to be extroverts, outgoing, and enjoy spending time with people and living life to the fullest. However, they can also be seen as competitive and unapologetic individuals.");
+});
+
+let orangeButton = document.createElement("button");
+orangeButton.innerText = "ORANGE";
+document.body.appendChild(orangeButton);
+orangeButton.style.backgroundColor = "#fa4";
+orangeButton.addEventListener("click", function() {
+    alert("The color of adventure and social communication.People who favor color orange are enthusiastic, extroverted, and they love attention. They are not very patient, but they deal well with life's setbacks. People who like orange color are usually very social and open to new experiences. They look at life as an adventure and they tend to be on a constant move. Their positive attitude and general enthusiasm for life is what attracts other people to them. They sometimes radiate such magnetism that everyone wants to be a part of their social circle. And they like it. They are not afraid of risk-taking, on the contrary, they enjoy it. ");
+});
+
+let yellowButton = document.createElement("button");
+yellowButton.innerText = "YELLOW";
+document.body.appendChild(yellowButton);
+yellowButton.style.backgroundColor = "#ffff00";
+yellowButton.addEventListener("click", function() {
+    alert("People who favor color yellow are confident, optimistic, cheerful. They are good communicators and they make friends easily. Even though, they prefer a small circle of close friends, rather than being involved in large social gatherings. Yellows are known as perfectionists. They have high expectations form themselves and from others. When those expectations are not met, yellows can become very critical and judgmental. Due to their short temper, they can be harsh on themselves and people around them.");
+});
+
+let greenButton = document.createElement("button");
+greenButton.innerText = "GREEN";
+document.body.appendChild(greenButton);
+greenButton.style.backgroundColor = "#00b300";
+greenButton.addEventListener("click", function() {
+    alert("Green personality type tends to be analytical, calm, logical, with a great thirst for knowledge. They are sometimes described as strategic thinkers and visionaries that are also born leaders. They like to be in charge and live their life on their own terms. Those traits sometimes make them appear as stubborn and even antisocial. They believe in science and base their decisions on facts. Additionally, their decisions always rule in favor of peacemaking.");
+});
+
+let blueButton = document.createElement("button");
+blueButton.innerText = "BLUE";
+document.body.appendChild(blueButton);
+blueButton.style.backgroundColor = "#3333ff";
+blueButton.addEventListener("click", function() {
+    alert("Somehow, the life purpose of those who like the color blue is linked with their ability to contribute to other people's well-being. People who favor this color are known to be confident, loyal, and calm. However, they can also evoke feelings of sadness and depression.");
+});
+
+let darkblueButton = document.createElement("button");
+darkblueButton.innerText = "DARKBLUE";
+document.body.appendChild(darkblueButton);
+darkblueButton.style.backgroundColor = "#0000b3";
+darkblueButton.addEventListener("click", function() {
+    alert("Indigo can boost up the perception a person has of things and encourage unconscious processes, such as intuition. This means that indigo can help develop a deeper awareness of what is going on. Darkblue is a color related to devotion and helping others. It suggests fairness and impartiality. The color has a deep a quality that transmits wisdom and authority.");
+});
+
+let purpleButton = document.createElement("button");
+purpleButton.innerText = "PURPLE";
+document.body.appendChild(purpleButton);
+purpleButton.style.backgroundColor = "#800080";
+purpleButton.addEventListener("click", function() {
+    alert("True to its red parent color, purple is often associated with luxury and power. However, as opposed to red’s tendency to reflect the material facets of power and ambition, purple errs towards royalty and nobility. The blue tones bring in a sense of relaxation and stability, which alongside the energy of red synthesizes to create a feeling of wisdom and good sense. In a similar vein, purple has a long-standing association with dignity. Purple is also commonly associated with mysticism and magic. This color can be used to impart a sense of the unknown, of curiosity and mystery. Similarly, purple often inspires creativity.");
+});
+
+
+
--- tema-6/tema6.js
+++ tema-6/tema6.js
@@ -1,6 +1,6 @@
 //ex. 1.1;
 
-let alertFunction = function() {
+let alertFunction = function () {
   alert("The first button was pressed.");
   return alertFunction;
 };
@@ -11,7 +11,7 @@ let alertFunction = function() {
 let button = document.createElement("button");
 button.innerText = "Second";
 
-button.addEventListener ("click", function() {
+button.addEventListener("click", function () {
   alert("The second button was pressed.");
 });
 document.body.appendChild(button);
@@ -19,14 +19,53 @@ document.body.appendChild(button);
 
 //ex. 1.3;
 
-let anotherButton = document.createElement("button");
-anotherButton.innerText = "Start";
-document.body.appendChild(anotherButton);
+let startButton = document.createElement("button");
+startButton.innerText = "Start";
+document.body.appendChild(startButton);
 
-let buttonFunction = function() {
-  let x = document.createTextNode("Step 1");
-  anotherButton.appendChild(x);
-  document.body.appendChild(anotherButton);
+function createButton(text) {
+  let newButton = document.createElement("button");
+  newButton.innerText = text;
+  document.body.appendChild(newButton);
+  return newButton;
 };
-document.addEventListener("click", buttonFunction);
 
+startButton.addEventListener("click", function () {
+  alert("Start");
+
+  let stepOneButton = createButton("Step 1");
+  stepOneButton.addEventListener("click", function () {
+    alert("Step 1");
+
+    let stepTwoButton = createButton("Step 2");
+    stepTwoButton.addEventListener("click", function () {
+      alert("Step 2");
+
+      let stepThreeButton = createButton("Step 3");
+      stepThreeButton.addEventListener("click", function () {
+        alert("Step 3");
+
+        let stepFourButton = createButton("Step 4");
+        stepFourButton.addEventListener("click", function () {
+          alert("Step 4");
+
+          let stepFiveButton = createButton("Step 5");
+          stepFiveButton.addEventListener("click", function () {
+            alert("You've reach the end, there are no more buttons");
+          });
+        });
+      });
+    });
+  });
+});
+
+
+// ex. 1.4;
+
+let getStartedButton = document.createElement("button");
+getStartedButton.innerText = "What's your favorite color?";
+document.body.appendChild(getStartedButton);
+
+getStartedButton.addEventListener("click", function() {
+  document.location.href = "../tema-6/tema6.1.html";
+});
