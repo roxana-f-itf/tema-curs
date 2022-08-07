625523bb31de09a34222a4f134e06d5ace65aee1
Tudor Goia <mindsetcoach360@gmail.com>
2022-08-07 19:30:15 +0300

    some home work fix

--- tema-10/tema10.js
+++ tema-10/tema10.js
@@ -5,9 +5,19 @@ let images = {
     },
 
     renderAllCrewImages: function() {
-        let imagesSection = document.querySelector('.imageContent.si-image')
+        // we get the parent for all the images
+        let imagesSection = document.querySelector('.imageContent > .si-image')
 
-        imagesSection.innerHTML += images.data.allCrewImages.image;
+        for (let i = 0; i < images.data.allCrewImages.length; i++) {
+            // create new divs, to act as a parents for each image element
+            // create new image elements (at each for step) - this can be done with document.createElement
+            // don;t forget to use .appendChild function, to put the img in the new divs and the new div in the 'imagesSection' element
+            imagesSection.innerHTML += images.data.allCrewImages[i].image;
+            // instead of putting the image source (.image property) in the innerHTML,
+            // make sure to put it in the 'src' HTML attribute
+            // use .setAttribute('src', images.data.allCrewImages[i].image)
+            // we did this in the project code
+        }
     },
     init: function() {
         axios
